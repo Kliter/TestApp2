e550cda941ce67829bf4efc5dc24398c5f0d4d3c
Katsumi Kusumi <katsumi.kusumi.ph@gmail.com>
2020-07-23 18:11:11 +0900

    Expand実装

--- app/src/main/java/com/kl/testapp2/listadapter/ListAdapter.kt
+++ app/src/main/java/com/kl/testapp2/listadapter/ListAdapter.kt
@@ -2,10 +2,15 @@ package com.kl.testapp2.listadapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.ListAdapter
+import androidx.transition.TransitionInflater
+import androidx.transition.TransitionManager
 import com.kl.testapp2.databinding.ItemUserBinding
 
 class ListAdapter : ListAdapter<User, UserViewHolder>(DiffUtil()) {
+
+    private var expandedIds = mutableSetOf<Int>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -16,6 +21,26 @@ class ListAdapter : ListAdapter<User, UserViewHolder>(DiffUtil()) {
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val data = getItem(position)
         holder.binding.user = data
-        // set onClickListeners
+        holder.binding.setOnClickItem {
+            val parent = holder.itemView.parent as? ViewGroup ?: return@setOnClickItem
+            val isExpanded = holder.binding.isExpanded ?: false
+            if (isExpanded) {
+                expandedIds.remove(data.id)
+            } else {
+                expandedIds.add(data.id)
+            }
+
+            val transition = TransitionInflater.from(holder.binding.root.context)
+                .inflateTransition(android.R.transition.move)
+            TransitionManager.beginDelayedTransition(parent, transition)
+            holder.binding.executeAfter {
+                this.isExpanded = !isExpanded
+            }
+        }
     }
+}
+
+inline fun <T : ViewDataBinding> T.executeAfter(block: T.() -> Unit) {
+    block()
+    executePendingBindings()
 }
